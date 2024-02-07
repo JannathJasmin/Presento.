e554f3208eb39ca40ab94d24c722fba6b292a5e9
Jannath Jasmin <jannathjasmin01@gmail.com>
2024-02-07 22:32:17 +0530

    Presento.

---
+++ Presento.js
@@ -0,0 +1,61 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayHoverPause:true,
+    dots:true,
+    loop:true,
+    margin:0,
+    
+       responsive:{
+            0:{
+                items:1
+            },
+            768:{
+              items:2
+            },
+            992:{
+                dotsEach:1,
+                items:6
+            },
+            1400:{
+              dotsEach:1,
+                items:6
+            }
+        }
+})
+  
+!function(d){var c="portfilter";var b=function(e){this.$element=d(e);this.stuff=d("[data-tag]");this.target=this.$element.data("target")||""};b.prototype.filter=function(g){var e=[],f=this.target;this.stuff.fadeOut("fast").promise().done(function(){d(this).each(function(){if(d(this).data("tag")==f||f=="all"){e.push(this)}});d(e).show()})};var a=d.fn[c];d.fn[c]=function(e){return this.each(function(){var g=d(this),f=g.data(c);if(!f){g.data(c,(f=new b(this)))}if(e=="filter"){f.filter()}})};d.fn[c].defaults={};d.fn[c].Constructor=b;d.fn[c].noConflict=function(){d.fn[c]=a;return this};d(document).on("click.portfilter.data-api","[data-toggle^=portfilter]",function(f){d(this).portfilter("filter")})}(window.jQuery);
+
+$(".counter").each(function () {
+  var $this = $(this),
+    countTo = $this.attr("data-countto");
+  countDuration = parseInt($this.attr("data-duration"));
+  $({ counter: $this.text() }).animate(
+    {
+      counter: countTo
+    },
+    {
+      duration: countDuration,
+      easing: "linear",
+      step: function () {
+        $this.text(Math.floor(this.counter));
+      },
+      complete: function () {
+        $this.text(this.counter);
+      }
+    }
+  );
+});
+
+
+$('.counter-count').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+    //chnage count up speed here
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
+});
